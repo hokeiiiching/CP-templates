a494cf1e5dd2764b067150c6fc491b6606a4f0e8
Ho Kei Ching <126402189+hokeiiiching@users.noreply.github.com>
2023-11-29 23:33:46 +0800

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,7 +1,16 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h>        
 using namespace std;
-using ll = long long;
-typedef vector<int> vi;
+using ll = long long;    // Common data types                        
+using vi = vector<int>;                                                                    
+using vll = vector<ll>;
+memset(memo, -1, sizeof memo);    // to init DP memo table
+vi memo(n, -1);    // Alternative way
+memset(arr, 0, sizeof arr);    // to clear array of ints
+//Shortcuts for "common" constants
+const int INF = 1e9;    // 10^9 = 1B is < 2^31 - 1, which is max size of int before int overflow
+const int LLINF = 4e18; // 4*10^18 is < 2^63 - 1, which is max size of ll before ll overflow
+const double EPS = 1e-9;    // a very small number
+
 #define PB push_back
 
 int main() {
