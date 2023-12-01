dd42fb865bdd9dfdfd3b79ca9bfeb40df6e1b657
Ho Kei Ching <126402189+hokeiiiching@users.noreply.github.com>
2023-12-01 20:34:34 +0800

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -3,12 +3,10 @@ using namespace std;
 using ll = long long;    // Common data types                        
 using vi = vector<int>;                                                                    
 using vll = vector<ll>;
-memset(memo, -1, sizeof memo);    // to init DP memo table
-vi memo(n, -1);    // Alternative way
-memset(arr, 0, sizeof arr);    // to clear array of ints
+
 //Shortcuts for "common" constants
 const int INF = 1e9;    // 10^9 = 1B is < 2^31 - 1, which is max size of int before int overflow
-const int LLINF = 4e18; // 4*10^18 is < 2^63 - 1, which is max size of ll before ll overflow
+const ll LLINF = 4e18; // 4*10^18 is < 2^63 - 1, which is max size of ll before ll overflow
 const double EPS = 1e-9;    // a very small number
 
 #define PB push_back
