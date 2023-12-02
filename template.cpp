dcaf275d9d1ceb910445dcb80c4f862201c93576
Ho Kei Ching <126402189+hokeiiiching@users.noreply.github.com>
2023-12-02 13:42:34 +0800

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -9,7 +9,7 @@ const int INF = 1e9;    // 10^9 = 1B is < 2^31 - 1, which is max size of int bef
 const ll LLINF = 4e18; // 4*10^18 is < 2^63 - 1, which is max size of ll before ll overflow
 const double EPS = 1e-9;    // a very small number
 
-#define PB push_back
+#define EB emplace_back
 
 int main() {
     ios::sync_with_stdio(0);
