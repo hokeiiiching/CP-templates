ecaefc5a4e671e94a4c9a741b9667e011fabc6e3
Ho Kei Ching <126402189+hokeiiiching@users.noreply.github.com>
2024-01-03 01:51:20 +0800

    Update sieveoferatosthenes.cpp

--- algorithms/sieveoferatosthenes.cpp
+++ algorithms/sieveoferatosthenes.cpp
@@ -1,12 +1,16 @@
-int sieve[n];
-sieve[0] = 1;
-sieve[1] = 1;
-for (int i = 2; i < n; ++i) {
-  sieve[i] = 0;
-} 
-for (int x = 2; x <= n; ++x) {
-  if (sieve[x]) continue;
-  for (int u = 2*x; u <= n; u += x) {
-    sieve[u] = x;
+void sieve() {
+  
+  int sieve[n];
+  sieve[0] = 1;
+  sieve[1] = 1;
+  for (int i = 2; i < n; ++i) {
+    sieve[i] = 0;
+  } 
+  for (int x = 2; x <= n; ++x) {
+    if (sieve[x]) continue;
+    for (int u = 2*x; u <= n; u += x) {
+      sieve[u] = x;
+    }
   }
+  
 }
